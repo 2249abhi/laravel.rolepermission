ff7a04c2be4f1a322b7de14d218b1a1bc9793e33
abhishek shrivastav <abhishekshr7@gmail.com>
2024-05-15 13:32:24 +0530

    changed login text

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,7 +36,7 @@
     <!-- ============================================================== -->
     <div class="splash-container">
         <div class="card ">
-            <div class="card-header text-center"><!--<a href="#"><img class="logo-img" src="{{ asset('assets/images/logo.png') }}" alt="logo"></a>--><span class="splash-description">{{ __('Cooperative') }}</span></div>
+            <div class="card-header text-center"><!--<a href="#"><img class="logo-img" src="{{ asset('assets/images/logo.png') }}" alt="logo"></a>--><span class="splash-description">{{ __('Role Permission') }}</span></div>
             <div class="card-body">
                 <form method="POST" action="{{ route('login') }}">
                     @csrf
